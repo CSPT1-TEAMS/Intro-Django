e66ac31cff17e9145abe9cc2a2109687ff5547c1
Charlie <ccariste@gmail.com>
2018-11-07 20:48:32 -0800

    CRC: saved note in day 1 test

--- notes/models.py
+++ notes/models.py
@@ -3,9 +3,9 @@ from uuid import uuid4
 
 # Create your models here.
 class Note ( models.Model ):
-    id = models.UUIDField ( primary_key = True, default = uuid4, editable = False )
+    id = models.UUIDField ( primary_key = True, default = uuid4, editable = False, blank = True )
     title = models.CharField( max_length = 30 )
     body = models.TextField( blank = True, max_length = 500)
-    author = models.CharField( max_length = 30 )
-    date = models.DateTimeField( auto_now=False, auto_now_add=False )
-    completed = models.BooleanField( default = Falses )
+    # author = models.CharField( max_length = 30, blank = True)
+    # date = models.DateTimeField( auto_now=False, auto_now_add=False, blank = True)
+    # completed = models.BooleanField( default = False, blank = True )
